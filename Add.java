0cbe78611dcad795442c7fd3db18a370cedb830f
Ankit Kumar <ankkumar29@publicisgroupe.net>
2021-04-27 09:42:16 +0530

    Multiplication added

--- Add.java
+++ Add.java
@@ -1,10 +1,10 @@
 public class Add{
 	public static void main(String args[]){
 		int n1;
-	int n2;
-	int n3;
-
-	System.out.println("1+2 =" + n1+n2);
+	        int n2;
+	        int n3=0;
+         n3= n1+n2;
+	System.out.println("1+2 =" + n3);
 	}
 
 }	
---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+public class Multiplication{
+	public static void main (String args[]){
+		
+		int n1=2;
+		int n2=3;
+		int res=0;
+
+		res= n1 * n2;
+
+		System.out.println("1*2 =" + res);
+	}
+}
+
