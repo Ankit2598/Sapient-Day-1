4fc03064183fcccc966f27cd1722fd80cd72fa85
Ankit2598 <36604725+Ankit2598@users.noreply.github.com>
2021-04-28 21:46:56 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,20 +1,13 @@
 public class Add{
 	public static void main(String args[]){
-<<<<<<< HEAD
 		int n1=5;
 	        int n2=8;
-	        int n3=0;
-         n3= n1+n2;
-	System.out.println("1+2 =" + n3);
-=======
-		int n1;
-	int n2;
-	int n3;
-        int Add= n1+n2;
-	int Sub= n1-n2;
+	        int Add=n1+n2;
+                int Sub=n1-n2;
+	
 	System.out.println("1+2 =" + Add);
 	System.out.println("1-2 =" + Sub);
->>>>>>> origin/feature_Branch
+
 	}
 
 }	
