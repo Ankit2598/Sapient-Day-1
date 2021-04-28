c3c794407b05eff9108703bd2466cb50d1e2c9ad
Ankit2598 <36604725+Ankit2598@users.noreply.github.com>
2021-04-28 09:03:02 +0530

    Update Add.java

--- Add.java
+++ Add.java
@@ -1,7 +1,7 @@
 public class Add{
 	public static void main(String args[]){
-		int n1;
-	        int n2;
+		int n1=5;
+	        int n2=8;
 	        int n3=0;
          n3= n1+n2;
 	System.out.println("1+2 =" + n3);
